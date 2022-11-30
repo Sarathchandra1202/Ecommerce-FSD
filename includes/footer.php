7997c82a7acddd1d44987fec8cae0a75c4163580
Sarathchandra1202 <sarathchandra7680@gmail.com>
2022-12-01 03:07:08 +0530

    FSD

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+      </div>
+      <strong>Made by Sarathchandra</strong>
+    </div>
+</footer>
